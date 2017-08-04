f86dc1d275eea3316f8ef599820656ad4d2f24f0
janelle-gatchalian <jgatchalian@getty.edu>
2017-08-03 18:08:11 -0700

    adjusted inline comments to say what code does

--- UV_index_bracelet_master.h
+++ UV_index_bracelet_master.h
@@ -1,10 +1,9 @@
 /***************************************************
-  This UV index bracelet was inspired by the Sunscreen reminder hat using the Si1145 UV/IR/Visible Light Sensor
-  visit https://learn.adafruit.com/sunscreen-reminder-hat for full tutorial
-  Written by Becky Stern for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution.
+  This UV index bracelet was inspired by the Sunscreen reminder hat,
+  written by Becky Stern for Adafruit Industries.
+  Visit https://learn.adafruit.com/sunscreen-reminder-hat for full tutorial.
 
-  EasyNeoPixels.h - Library for making neopixels more approachable.
+  EasyNeoPixels.h
   Created by Evelyn Masso, April 9, 2017.
 
   UV index bracelet
@@ -18,21 +17,21 @@
 Adafruit_SI1145 uv = Adafruit_SI1145();
 
 uint32_t sinceTime;
-uint32_t markerTime; //start time, usually 0
+uint32_t markerTime; // start time, usually 0
 boolean shouldLight = false;
 float UVindex;
-float lowUVthreshold = .01;
+float lowUVthreshold = .01; // Neopixel light blinks green by default. Change thresholds as necessary.
 float mediumUVthreshold = 7.1;
 float highUVthreshold = 10.1;
-//.01-7.0 Green light. Moderate to high risk of UV rays.
-//7.1-10.0 Yellow light. Very high risk of UV rays.
-//10.1-12 Red light. Extremely high risk of UV rays.
-uint32_t reapplyInterval = 900000; // 15 minutes = 900000 milliseconds. One hour = 3600000 milliseconds. Two hours = 7200000 milliseconds
+// .01-7.0 Green light. Moderate to high risk of UV rays per World Health Organization.
+// 7.1-10.0 Yellow light. Very high risk of UV rays per World Health Organization.
+// 10.1-12 Red light. Extremely high risk of UV rays per World Health Organization.
+uint32_t reapplyInterval = 900000; // Neopixel light blinks every 15 minutes = 900000 milliseconds. One hour = 3600000 milliseconds. Two hours = 7200000 milliseconds
 
 void setup() {
   setupEasyNeoPixels(12, 1);
   Serial.begin(9600);
-  Serial.println("Adafruit SI1145 test");
+  Serial.println("Adafruit SI1145 test"); // Registers when UV index sensor comes up with an index reading
   if (! uv.begin()) {
     Serial.println("Didn't find Si1145");
     while (1);
@@ -43,17 +42,17 @@ void setup() {
 void loop() {
   Serial.println("===================");
   UVindex = uv.readUV();
-  UVindex /= 100.0; // the index is multiplied by 100 so to get the integer index, divide by 100!
+  UVindex /= 100.0; // The index is multiplied by 100, so to get the integer index, divide by 100!
   Serial.print("UV: ");  Serial.println(UVindex);
   Serial.print("Seconds until next alert: ");  Serial.println((reapplyInterval - sinceTime) / 1000);
   delay(1000);
 
-  sinceTime = millis() - markerTime; //how long since last "mark"
+  sinceTime = millis() - markerTime; // how long since last "mark"
 
-  if (UVindex >= highUVthreshold && shouldLight) { //only light when we are currently outside
+  if (UVindex >= highUVthreshold && shouldLight) { // only light when we are currently outside
     for (int i = 0; i <= 3; i++) {
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 180, 0, 0); //(neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 180, 0, 0); // (neopixel number, R value, G value, B value)
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 180, 0, 0);
@@ -64,10 +63,10 @@ void loop() {
     shouldLight = false;
     resetTimer();
   }
-  else if (UVindex >= mediumUVthreshold && shouldLight) { //only light when we are currently outside
+  else if (UVindex >= mediumUVthreshold && shouldLight) { // only light when we are currently outside
     for (int i = 0; i <= 3; i++) {
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 255, 200, 0); //(neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 255, 200, 0); // (neopixel number, R value, G value, B value)
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 255, 200, 0);
@@ -78,9 +77,9 @@ void loop() {
     shouldLight = false;
     resetTimer();
   }
-  else if (UVindex >= lowUVthreshold && shouldLight) { //only light when we are currently outside
+  else if (UVindex >= lowUVthreshold && shouldLight) { // only light when we are currently outside
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 0, 180, 0); //(neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 0, 180, 0); // (neopixel number, R value, G value, B value)
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 0, 180, 0);
@@ -103,7 +102,7 @@ void loop() {
     shouldLight = false;
     resetTimer();
   }
-  if (sinceTime > reapplyInterval) { //check to see if we've exceeded the time limit
+  if (sinceTime > reapplyInterval) { // check to see if we've exceeded the time limit
     shouldLight = true;
     resetTimer();
   }
