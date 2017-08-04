b7e64cb72b75b374f9fb03e5d0a1297f4511a6ab
janelle-gatchalian <jgatchalian@getty.edu>
2017-08-03 18:35:14 -0700

    adjusted inline comments to say what code does

--- UV_index_bracelet_master.h
+++ UV_index_bracelet_master.h
@@ -20,18 +20,19 @@ uint32_t sinceTime;
 uint32_t markerTime; // start time, usually 0
 boolean shouldLight = false;
 float UVindex;
-float lowUVthreshold = .01; // Neopixel light blinks green by default. Change thresholds as necessary.
+float lowUVthreshold = .01; // Neopixel light blinks green by default when the UV index sensor comes up with a UV index reading. Change thresholds to change colors as necessary.
 float mediumUVthreshold = 7.1;
 float highUVthreshold = 10.1;
-// .01-7.0 Green light. Moderate to high risk of UV rays per World Health Organization.
-// 7.1-10.0 Yellow light. Very high risk of UV rays per World Health Organization.
-// 10.1-12 Red light. Extremely high risk of UV rays per World Health Organization.
+// Bracelet colors follow colors of the stoplight.
+// .01-7.0 Green light for low threshold. Moderate to high risk of UV rays per World Health Organization.
+// 7.1-10.0 Yellow light for medium threshold. Very high risk of UV rays per World Health Organization.
+// 10.1-12 Red light for high threshold. Extremely high risk of UV rays per World Health Organization.
 uint32_t reapplyInterval = 900000; // Neopixel light blinks every 15 minutes = 900000 milliseconds. One hour = 3600000 milliseconds. Two hours = 7200000 milliseconds
 
 void setup() {
   setupEasyNeoPixels(12, 1);
   Serial.begin(9600);
-  Serial.println("Adafruit SI1145 test"); // Registers when UV index sensor comes up with an index reading
+  Serial.println("Adafruit SI1145 test"); // Message registers on computer when UV index sensor comes up with a UV index reading
   if (! uv.begin()) {
     Serial.println("Didn't find Si1145");
     while (1);
@@ -52,7 +53,7 @@ void loop() {
   if (UVindex >= highUVthreshold && shouldLight) { // only light when we are currently outside
     for (int i = 0; i <= 3; i++) {
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 180, 0, 0); // (neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 180, 0, 0); // (neopixel number, R value, G value, B value) This where you specify color.
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 180, 0, 0);
@@ -66,7 +67,7 @@ void loop() {
   else if (UVindex >= mediumUVthreshold && shouldLight) { // only light when we are currently outside
     for (int i = 0; i <= 3; i++) {
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 255, 200, 0); // (neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 255, 200, 0); // (neopixel number, R value, G value, B value) This is where you specify color.
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 255, 200, 0);
@@ -79,7 +80,7 @@ void loop() {
   }
   else if (UVindex >= lowUVthreshold && shouldLight) { // only light when we are currently outside
       digitalWrite(12, HIGH);
-      writeEasyNeoPixel(0, 0, 180, 0); // (neopixel number, R value, G value, B value)
+      writeEasyNeoPixel(0, 0, 180, 0); // (neopixel number, R value, G value, B value) This is where you specify color.
       delay(250);
       digitalWrite(12, LOW);
       writeEasyNeoPixel(0, 0, 180, 0);
